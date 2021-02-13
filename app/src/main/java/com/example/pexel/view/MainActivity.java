ff46d3cccfda4a6a56cdb7216788e1b0e083ada9
Abhishek Yadava <abhishek.yadava@robosoftin.com>
2021-02-13 20:35:46 +0530

    updated banner image

--- app/src/main/java/com/example/pexel/view/MainActivity.java
+++ app/src/main/java/com/example/pexel/view/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import com.example.pexel.R;
@@ -35,10 +37,13 @@ public class MainActivity extends AppCompatActivity implements OnItemClick {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         initialization();
-
         getPexelPhotos();
     }
 
